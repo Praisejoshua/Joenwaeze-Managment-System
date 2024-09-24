ef5601813064c61522e7cdb1e9a0666452d20763
Joshua Praise <praizjoshua263@gmail.com>
2024-09-24 13:57:24 +0100

    Update requirements and ensure migrations are up to date3

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JoenwaezeManagmentSystem.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JoenwaezeManagementSystem.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
